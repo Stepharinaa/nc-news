bf1af84d1bbc007721bb3722d476aee08ae3f021
Stepharinaa <stephanielplee@gmail.com>
2025-03-07 10:19:49 +0000

    remove converttimestampttodate function

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,20 +1,13 @@
 const db = require("../../db/connection");
-const format = require("pg-format")
-
-/*exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
-  if (!created_at) return { ...otherProperties };
-  return { created_at: new Date(created_at), ...otherProperties };
-}; 
-^ this is no longer needed due to the new Date constructor we've used in seed.js
-*/ 
+const format = require("pg-format");
 
 const mapCommentsToArticleIds = (commentData, articleData) => {
   const articleIdLookup = articleData.reduce((lookup, article) => {
-    lookup[article.title] = article.article_id; 
+    lookup[article.title] = article.article_id;
     return lookup;
   }, {});
 
-  console.log("Article ID Lookup:", articleIdLookup); 
+  console.log("Article ID Lookup:", articleIdLookup);
 
   return commentData.map(({ article_title, ...rest }) => {
     if (!articleIdLookup[article_title]) {
@@ -23,7 +16,7 @@ const mapCommentsToArticleIds = (commentData, articleData) => {
 
     return {
       ...rest,
-      article_id: articleIdLookup[article_title], 
+      article_id: articleIdLookup[article_title],
     };
   });
 };
