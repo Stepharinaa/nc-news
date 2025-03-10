f320293f7fde6999bcfa1183b60306678667d1ad
Stepharinaa <stephanielplee@gmail.com>
2025-03-10 14:25:27 +0000

    add error handling if article doesn't exist to models AND converts created_at ISO string to a timestamp

--- model/models.js
+++ model/models.js
@@ -6,10 +6,16 @@ const fetchTopics = () => {
   });
 };
 
-const fetchArticlebyArticleID = (articleID) => {
+const fetchArticlebyArticleID = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleID])
-    .then(({ rows }) => rows[0]);
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      if (!rows.length) return null;
+      const article = rows[0];
+
+      // Need to convert created_At string to be timestamp:
+      return { ...article, created_at: new Date(article.created_at).getTime() };
+    });
 };
 
 module.exports = { fetchTopics, fetchArticlebyArticleID };
