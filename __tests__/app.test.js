48c43dff0b49ce40cd082bd78e9d3c8fcf012693
Stepharinaa <stephanielplee@gmail.com>
2025-03-13 10:52:33 +0000

    adjust created_at test to ensure it is returned as a string as per Alex's comment

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -66,7 +66,7 @@ describe("GET /api/articles", () => {
           expect(typeof article.title).toBe("string");
           expect(typeof article.article_id).toBe("number");
           expect(typeof article.topic).toBe("string");
-          expect(typeof article.created_at).toBe("number");
+          expect(typeof article.created_at).toBe("string");
           expect(typeof article.votes).toBe("number");
           expect(typeof article.article_img_url).toBe("string");
           expect(typeof article.comment_count).toBe("string");
@@ -197,7 +197,7 @@ describe("GET /api/articles", () => {
           expect(typeof article.topic).toBe("string");
           expect(typeof article.author).toBe("string");
           expect(typeof article.body).toBe("string");
-          expect(typeof article.created_at).toBe("number");
+          expect(typeof article.created_at).toBe("string");
           expect(typeof article.votes).toBe("number");
           expect(typeof article.article_img_url).toBe("string");
         });
@@ -236,7 +236,7 @@ describe("GET /api/articles/:articleid/comments", () => {
         comments.forEach((comment) => {
           expect(typeof comment.comment_id).toBe("number");
           expect(typeof comment.votes).toBe("number");
-          expect(typeof comment.created_at).toBe("number");
+          expect(typeof comment.created_at).toBe("string");
           expect(typeof comment.author).toBe("string");
           expect(typeof comment.body).toBe("string");
           expect(typeof comment.article_id).toBe("number");
