6b2ef1ec90f9598f45e08de23432f8193a385dab
Stepharinaa <stephanielplee@gmail.com>
2025-03-14 16:43:17 +0000

    add 23502 PSQL error in error-handling-controllers file

--- controllers/error-handling-controllers.js
+++ controllers/error-handling-controllers.js
@@ -1,6 +1,8 @@
 const handlePSQLErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
-    res.status(400).send({ msg: "bad request..." });
+    res.status(400).send({ msg: "Bad request: Invalid data type..." });
+  } else if (err.code === "23502") {
+    res.status(400).send({ msg: "Bad Request: Missing required fields..." });
   } else {
     next(err);
   }
