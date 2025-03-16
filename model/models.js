9e4ebc4d3e4904b7c6b8a4af554691e31f274921
Stepharinaa <stephanielplee@gmail.com>
2025-03-16 16:07:05 +0000

    move error handling to users-controller.js file

--- model/models.js
+++ model/models.js
@@ -257,9 +257,6 @@ const fetchUserByUsername = (username) => {
   return db
     .query(`SELECT * FROM users WHERE username = $1`, [username])
     .then(({ rows }) => {
-      if (!rows.length) {
-        return Promise.reject({ status: 404, msg: "username not found..." });
-      }
       return rows[0];
     });
 };
